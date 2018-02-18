65bc95c16e00c4a0693bb15879412caf5699a092
amit chaturvedi <eramit95@gmail.com>
2018-02-18 20:22:35 +0530

    Adding like button functionality
    clicking the like updates the number of likes of particular Idea.

--- ideative/src/app/idea/idea.component.ts
+++ ideative/src/app/idea/idea.component.ts
@@ -9,9 +9,14 @@ export class IdeaComponent implements OnChanges {
 
   @Input() ideaTitle: string;
   @Input() ideaDescription: string;
+  @Input() likeCount: number = 0;
   dummyDate = new Date();
 
   constructor() { }
+
+  onClickingLike():any {
+    this.likeCount++;
+  }
 
   ngOnChanges() {
   }
