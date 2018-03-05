978204b6fc52e5614b087cea809dbf079996d4e5
Harshit Kaushik <harshit@kwench.in>
2018-03-05 23:42:46 +0530

    settings component basic panels

--- ideative/src/app/settings/settings.component.ts
+++ ideative/src/app/settings/settings.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
+  chosenSettingsName = 'General Settings';
+
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  setChosenSettings(menuId: number): void {
+    if (menuId === 1) {
+      this.chosenSettingsName = 'General Settings';
+    } else if (menuId === 2) {
+      this.chosenSettingsName = 'Emails & Notifs';
+    } else if (menuId === 3) {
+      this.chosenSettingsName = 'Connected Services';
+    } else if (menuId === 4) {
+      this.chosenSettingsName = 'Terms & License';
+    }
   }
 
 }
